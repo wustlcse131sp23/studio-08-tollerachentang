d1269a18b35a889e41ce391bcb8c62540888f99e
17163 <17163@DESKTOP-43U96KU.dhcp.wustl.edu>
2023-04-06 15:51:47 -0500

    GOOD JOB!!

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,81 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Date {
+	private int month;
+	private int day;
+	private int year;
+	private boolean isHoliday;
 
+	/**
+	 * it's a constructor making an object in Date class with following parameters
+	 * @param day
+	 * @param month
+	 * @param year
+	 * @param isHoliday
+	 */
+	public Date(int month, int day, int year, boolean isHoliday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.isHoliday = isHoliday;
+	}
 
-    public static void main(String[] args) {
+	public String toString() {
+		if(isHoliday) {
+		return month + " " + day + ", " + year + " is a holiday.";}
+		else {
+			return month + " " + day + ", " + year + " is not a holiday.";
+		}
+		
+	}
+	
+	
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + day;
+		result = prime * result + month;
+		result = prime * result + year;
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		if (day != other.day)
+			return false;
+		if (month != other.month)
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
+
+	
+	public static void main(String[] args) {
+    	Date d1 = new Date (4, 6, 2023, false);
+    	Date d2 =  new Date (4, 6, 2023, false);
+    	LinkedList<Date> list = new LinkedList<Date>();
+    	list.add(d1);
+    	list.add(d2);
+    	list.add(d1);
+    	System.out.println(list);
+    	
+    	HashSet<Date> set = new HashSet<Date>();
+    	set.add(d1);
+    	set.add(d2);
+    	set.add(d1);
+    	System.out.println(set);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,10 +1,67 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	
+	private int hours;
+	private int minutes;
 
+	
+	/**
+	 * it's a constructor making an object in Time class with 
+	 * @param hour
+	 * @param minute
+	 */
+	public Time (int hour, int minute) {
+		this.hours = hour;
+		this.minutes = minute;
+	}
+
+	
+	
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + hours;
+		result = prime * result + minutes;
+		return result;
+	}
+
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		if (hours != other.hours)
+			return false;
+		if (minutes != other.minutes)
+			return false;
+		return true;
+	}
+
+
+
+
+
+	public String toString () {
+		return "" + hours + ":" + minutes;
+	}
+	
 	public static void main(String[] args) {
- 
+		Time t1 = new Time (15, 03);
+		System.out.println(t1);
     	
     }
 
